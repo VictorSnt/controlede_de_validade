cc3b96303f158ece12b54e82a8db2ea6c1097347
victor <victoorsantos266@gmail.com>
2024-01-26 23:10:34 -0300

    done

--- controle_validades/expiration_control/models.py
+++ controle_validades/expiration_control/models.py
@@ -2,3 +2,4 @@ from .model.controle_validades.comissao import Comissao
 from .model.controle_validades.product import Product
 from .model.controle_validades.validade import Validade
 from .model.aleterdata.detalhe import Detalhe
+from .model.aleterdata.produto import Produto
--- controle_validades/expiration_control/tasks.py
+++ controle_validades/expiration_control/tasks.py
@@ -1,36 +1,39 @@
 from .utils.email import EmailHandler
-from .models import Validade, Detalhe
+from .models import Validade, Detalhe, Produto
 from background_task import background
 from datetime import datetime, timedelta
 
 
-@background(schedule=10)
-def notify_expiration(): 
-    input('email')
-    validades = Validade.objects.all().order_by('dtvalidade')
-    today = datetime.today().date()
-    limit_day = today + timedelta(days=7)
-    vencidos = [val for val in validades if val.dtvalidade <= limit_day]
-    recipients = ['thanatsuv3@gmail.com']
-    subject = 'Produtos Vencidos e proximos de vencer'
-    email = EmailHandler(recipients, vencidos, today, subject)
-    email.send_email()
+# @background(schedule=1000)
+# def notify_expiration(): 
+#     print('email')
+#     validades = Validade.objects.all().order_by('dtvalidade')
+#     today = datetime.today().date()
+#     limit_day = today + timedelta(days=7)
+#     vencidos = [val for val in validades if val.dtvalidade <= limit_day]
+#     recipients = ['thanatsuv3@gmail.com']
+#     subject = 'Produtos Vencidos e proximos de vencer'
+#     email = EmailHandler(recipients, vencidos, today, subject)
+#     email.send_email()
     
 
-@background(schedule=10)
+@background(schedule=1000)
 def add_promo_tag():
     print('begin')
     today = datetime.today().date()
-    validades = Validade.objects.filter(dtvalidade__lte=today)[0:1]
+    validades = Validade.objects.filter(dtvalidade__lte=today)
     for validade in validades:
+        if '(PROMO)' in validade.produto.dsdetalhe:
+            print('have')
+            continue
         iddetalhe = validade.produto.iddetalhe
         detalhe = Detalhe.objects.using('alterdata').get(iddetalhe=iddetalhe)
-        detalhe.dsdetalhe += ' (PROMO)' 
+        produto = Produto.objects.using('alterdata').get(idproduto=detalhe.idproduto)
+        detalhe.dsdetalhe += ' (PROMO)'
+        produto.nmproduto = detalhe.dsdetalhe
         detalhe.save()
-    print(detalhe)
-    test = Detalhe.objects.using('alterdata').get(iddetalhe=iddetalhe)
-    input(test.dsdetalhe)
-
+        produto.save()
+    print('done')
 add_promo_tag()
 
 
